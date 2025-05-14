441127c8203c413ffdf702fdd2d3d6b8dafa6c56
epimix <epimiks@gmail.com>
2025-05-14 20:39:26 +0300

    main page (of adding transaction with selecting category adding note)

--- TeamProject/MainWindow.xaml.cs
+++ TeamProject/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.Collections.ObjectModel;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -11,20 +12,14 @@ using System.Windows.Shapes;
 
 namespace TeamProject
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+
+
         public MainWindow()
         {
             InitializeComponent();
-        }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            MessageBox.Show("work!");
-            Console.WriteLine("asdsd");
         }
     }
 }
