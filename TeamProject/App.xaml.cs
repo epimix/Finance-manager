97f2112cee2e8559916543b18d4bdbf67d629f83
Kelwoi <vova0110gordienko@gmail.com>
2025-05-15 20:21:27 +0300

    database_created

--- TeamProject/App.xaml.cs
+++ TeamProject/App.xaml.cs
@@ -1,6 +1,8 @@
-﻿using System.Configuration;
+﻿using Microsoft.EntityFrameworkCore;
+using System.Configuration;
 using System.Data;
 using System.Windows;
+using TeamProject.Database;
 
 namespace TeamProject
 {
@@ -9,6 +11,20 @@ namespace TeamProject
     /// </summary>
     public partial class App : Application
     {
+        public static FinanceDbContext Db { get; private set; }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            var options = new DbContextOptionsBuilder<FinanceDbContext>()
+                .UseSqlite("Data Source=finance.db")
+                .Options;
+
+            Db = new FinanceDbContext(options);
+
+            Db.Database.EnsureCreated();
+
+            base.OnStartup(e);
+        }
     }
 
 }
