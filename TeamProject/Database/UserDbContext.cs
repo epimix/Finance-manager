3d066a904e750b7c2696825ad3a7ee89628f4b7b
Kelwoi <vova0110gordienko@gmail.com>
2025-05-16 19:06:21 +0300

    database_user_update

---
+++ TeamProject/Database/UserDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TeamProject.Entities;
+
+namespace TeamProject.Database
+{
+    public class UserDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public UserDbContext(DbContextOptions<UserDbContext> options) : base(options)
+        {
+        }
+    }
+}
